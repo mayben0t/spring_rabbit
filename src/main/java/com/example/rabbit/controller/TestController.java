65b0e3f4cc543231c6492225783b7dd6c50d4e03
王骁 <wangxiao@koolearn.com>
2019-09-09 21:01:57 +0800

    dead_letter

--- src/main/java/com/example/rabbit/controller/TestController.java
+++ src/main/java/com/example/rabbit/controller/TestController.java
@@ -3,6 +3,7 @@ package com.example.rabbit.controller;
 
 import com.example.rabbit.model.User;
 import org.springframework.amqp.core.DirectExchange;
+import org.springframework.amqp.core.Exchange;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
@@ -19,11 +20,23 @@ public class TestController {
     @Qualifier("testExchange")
     private DirectExchange directExchange;
 
+    @Autowired
+    @Qualifier("deadLetterExchange")
+    private Exchange deadLetterExchange;
+
     @GetMapping("/hi")
     public String test(){
         User user = new User(1,"test",22);
 
         rabbitTemplate.convertAndSend(directExchange.getName(),"",user);
+        return "hi";
+    }
+
+    @GetMapping("/hic")
+    public String testc(){
+        User user = new User(1,"test",22);
+
+        rabbitTemplate.convertAndSend(deadLetterExchange.getName(),"DL_KEY",user);
         return "hi";
     }
 }
--- src/main/java/com/example/rabbit/rabbitConfig/RabbitConfig.java
+++ src/main/java/com/example/rabbit/rabbitConfig/RabbitConfig.java
@@ -1,9 +1,18 @@
 package com.example.rabbit.rabbitConfig;
 
+import com.google.common.collect.Maps;
 import com.rabbitmq.client.AMQP;
+import org.springframework.amqp.core.Binding;
+import org.springframework.amqp.core.DirectExchange;
+import org.springframework.amqp.core.Exchange;
+import org.springframework.amqp.core.ExchangeBuilder;
 import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.core.QueueBuilder;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.Map;
 
 @Configuration
 public class RabbitConfig {
@@ -16,5 +25,29 @@ public class RabbitConfig {
     @Bean("testQueuez")
     public Queue getQueue2(){
         return new Queue("testQueuez");
+    }
+
+
+    @Bean("deadLetterExchange")
+    public Exchange deadLetterExchangeGet(){
+        return ExchangeBuilder.directExchange("deadLetterExchange").durable(true).build();
+    }
+
+    @Bean("deadLetterQueue")
+    public Queue deadLetterQueueGet(@Qualifier("deadLetterExchange")Exchange exchange){
+        Map<String,Object> args = Maps.newHashMap();
+        args.put("x-dead-letter-exchange",exchange.getName());
+        args.put("x-dead-letter-routing-key","KEY_D");
+        return QueueBuilder.durable("deadLetterQueue").withArguments(args).build();
+    }
+
+    @Bean
+    public Binding deadLetterBinding(){
+        return new Binding("deadLetterQueue",Binding.DestinationType.QUEUE,"deadLetterExchange","DL_KEY",null);
+    }
+
+    @Bean
+    public Binding redirectBinding(){
+        return new Binding("testQueuez",Binding.DestinationType.QUEUE,"deadLetterExchange","KEY_D",null);
     }
 }
